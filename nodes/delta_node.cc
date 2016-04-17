5085e7f9601c53d1ac9954622239183bad7a952f
Stan Swidwinski <stanislaw.swidwinski@yale.edu>
2016-04-17 00:54:47 -0400

    All delta types handled

--- nodes/delta_node.cc
+++ nodes/delta_node.cc
@@ -3,3 +3,69 @@
 */
 
 #include "nodes/delta_node.h"
+
+DeltaNode::DeltaNode (NodeType type,
+		Pair<int, byte*>* newValue,
+		PID nextNode) : Node(type) {
+	setVariables(type, newValue, nextNode);
+}
+
+DeltaNode::DeltaNode (NodeType type,
+		PID nextNode,
+		PID pter,
+		int splitKey,
+		int borderKey) : Node(type) {
+	setVariables(type, nextNode, pter, splitKey, borderKey);
+}
+
+void DeltaNode::setVariables(NodeType type,
+		Pair<int, byte*>* newValue,
+		PID nextNode) {
+	Node::type_ = type;
+	newValue_ = newValue;
+	nextNode_ = nextNode;
+	// those are not used.
+	pter_ = -1;
+	splitKey_ = -1;
+	borderKey_ = -1;
+}
+
+void DeltaNode::setVariables(NodeType type,
+		PID nextNode,
+		PID pter,
+		int splitKey,
+		int borderKey) {
+	
+	Node::type_ = type;
+
+	nextNode_ = nextNode;
+	pter_ = pter;
+	splitKey_ = splitKey;
+	borderKey_ = borderKey;
+
+	// those are not used.
+	newValue_ = nullptr;
+}
+
+
+//@ TODO
+// taking a side pointer when the node is DELTA_SPLIT
+// means that the SMO must be finished. Call this here.
+PID DeltaNode::nextPID(int key) {
+	if(Node::type_ == DELTA_SPLIT) {
+		if(key <= borderKey_ &&
+			key > splitKey_) {
+
+			return pter_;
+		}
+
+		return nextNode_;
+	} else if (Node::type_ == DELTA_INDEX_SPLIT) {
+		if(key > splitKey_) 
+			return pter_;
+
+		return nextNode_;
+	} else {
+		return nextNode_;
+	}
+}
--- nodes/delta_node.h
+++ nodes/delta_node.h
@@ -7,15 +7,69 @@
 
 #include "nodes/node.cc"
 #include "utils/pair.h"
+#include "utils/common.h"
 
 class DeltaNode : public Node {
 	public:
+		// full initialization -- update, insert, delete.
+		DeltaNode(NodeType type,
+			Pair<int, byte*>* newValue,
+			PID nextNode);
+
+		// full initialization -- split deltas.
+		DeltaNode(NodeType type,
+			PID nextNode,
+			PID pter,
+			int splitKey,
+			int borderKey = -1);
+
+		// set values -- update, insert, delete.
+		void setVariables(NodeType type,
+			Pair<int, byte*>* newValue,
+			PID nextNode);
+
+		// set values -- update, insert, delete.
+		void setVariables(NodeType type,
+			PID nextNode,
+			PID pter,
+			int splitKey,
+			int borderKey = -1);
+
+		// for buffer initialization
 		DeltaNode() : Node(DELTA_INSERT) {}
 
+		// for insert, update, delete
+		int getNewKey() {
+			return newValue_->key;
+		}
+
+		// for insert, update
+		byte* getNewValue() {
+			return newValue_->value;
+		}
+
+		// for split nodes.
+		PID nextPID(int key);
+
 	private:
-		// All deltas have a pair representing data. Delete deltas
-		// will have a non-initialized value element.
-		Pair<int, int> value;
+		// update deltas store new value for given key.
+		// insert deltas store new value and key
+		// delete deltas store only meaningful key
+		Pair<int, byte*>* newValue_;
+
+		// PID of the next node we are pointing to
+		PID nextNode_;
+
+		// In split deltas -- side pointer.
+		// In index split deltas -- 
+		PID pter_;
+
+		// for split deltas -- splits P and Q (see paper)
+		// for index split deltas -- same.
+		int splitKey_;
+
+		// for index split deltas -- deliminates Q. (see paper)
+		int borderKey_;
 };
 
 #endif
