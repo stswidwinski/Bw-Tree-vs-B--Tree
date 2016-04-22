c650ef873367d38a5926d3d1e073c25e3dc5370b
Stan Swidwinski <stanislaw.swidwinski@yale.edu>
2016-04-22 13:28:54 -0400

    Organization and split variables

--- utils/common.h
+++ utils/common.h
@@ -24,21 +24,44 @@
     exit(1); \
   } while (0);
 
-#define KEY_NOT_SET -1
-// the length of the entry.
-#define ENTRY_LEN 8
+/*
+	Constants necessary for data seek inside index node.
+*/
+
 // flag which means that PID was not found
 #define PID_NOT_FOUND -100
-// flag which means that record doesn't exist
+// maximal number of keys in index nodes
+#define MAX_KEYS 256
+
+/*
+	Constants necessary for data seek inside data node.
+*/
+
+// the length of the entry.
+#define ENTRY_LEN 8
+// maximal number of data pairs inside data node
+#define MAX_RECORDS 1024
+
+// flag which means that record was not found
 #define NOT_FOUND -1
  // flag which means that record is above range 
 #define OVER_HIGH -2
 // flag which means that record is found 
-#define FOUND -102
+#define FOUND 0
+
+/*
+	Constants necessary for data seek inside index node.
+*/
+// max key inside index node is not set
+#define KEY_NOT_SET -1
+
+/*
+	Delta chain indices
+*/
+
 // maximal length of the delta chain
 #define MAX_DELTA_CHAIN 100
-// maximal number of keys in index nodes
-#define MAX_KEY 1024
+
 
 typedef int PID;
 typedef unsigned char byte;
