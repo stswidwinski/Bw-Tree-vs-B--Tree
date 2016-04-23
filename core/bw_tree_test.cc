cd690e70736b6eb62ac88c5c49b700054821b20b
Stan Swidwinski <stanislaw.swidwinski@yale.edu>
2016-04-23 16:57:59 -0400

    Does not compile yet

--- core/bw_tree_test.cc
+++ core/bw_tree_test.cc
@@ -9,8 +9,50 @@ TEST(populateTest) {
   END;
 }
 
-TEST(consolidateTest) {
+TEST(dataNodeInsertConsolidateTest) {
+	BwTree* t = BwTree();
+	// give memory manager only data nodes.
+	MemoryManager man = MemoryManager(MAX_DELTA_CHAIN,
+		0, 0);
 
+	// insert only to the initial right kid.
+	int initialKey = INIT_KEY_VALUE + 1;
+
+	// payload has monotinically increasing 
+	// value from i to i+LENGTH_RECORDS
+	byte* payload;
+
+	// insert up to MAX_DELTA_CHAIN into the chain
+	for(int i = 0; i < MAX_DELTA_CHAIN; i++){
+		for(int j = 0; j < LENGTH_RECORDS; j++)
+			*(payload + j) = (byte) i + j;
+		t->insert(i, b, &man);
+	}
+
+	// check the whole damn thing. We should have 
+	// MAX_DELTA_CHAIN insert deltas.
+	//
+	// get the first in chain as indicated by findNode.
+	Node* currNode = t->findNode(MAX_DELTA_CHAIN - 1,
+		&man).node;
+
+	// check that it is the correct node.
+	Node* root = t->map_->get(t->rootPid_);
+	Node* firstInChain = ((IndexNode*) root)->getIndexPid(0);
+	EXPECT_EQ(firstInChain, currNode);
+
+	byte* foundPayload;
+	for(int i = MAX_DELTA_CHAIN - 1; i >= 0; i--) {
+		// every node in chain must be a insert delta
+		EXPECT_EQ(NodeType::DELTA_INSERT, currNode->getType());
+		// every node must contain the right key and payload
+		EXPECT_EQ(i, ((DeltaNode*) currNode)->getKey());
+		foundPayload = ((DeltaNode*) currNode)->getValue();
+		for(int j = 0; j < LENGTH_RECORDS; j++)
+			EXPECT_EQ((byte) i + j, *(foundPayload + j));
+	}
+
+	// TODO insert the last node that will trigger consolidation.
 
 	END;
 }
