1bca525ddc7bb2b1dd32e0e795894c3c1404095b
Stan Swidwinski <stanislaw.swidwinski@yale.edu>
2016-04-23 17:13:41 -0400

    test

--- core/bw_tree_test.cc
+++ core/bw_tree_test.cc
@@ -10,7 +10,7 @@ TEST(populateTest) {
 }
 
 TEST(dataNodeInsertConsolidateTest) {
-	BwTree* t = BwTree();
+	BwTree t = BwTree();
 	// give memory manager only data nodes.
 	MemoryManager man = MemoryManager(MAX_DELTA_CHAIN,
 		0, 0);
@@ -20,25 +20,25 @@ TEST(dataNodeInsertConsolidateTest) {
 
 	// payload has monotinically increasing 
 	// value from i to i+LENGTH_RECORDS
-	byte* payload;
+	byte* payload = new byte[LENGTH_RECORDS];
 
 	// insert up to MAX_DELTA_CHAIN into the chain
 	for(int i = 0; i < MAX_DELTA_CHAIN; i++){
 		for(int j = 0; j < LENGTH_RECORDS; j++)
 			*(payload + j) = (byte) i + j;
-		t->insert(i, b, &man);
+		t.insert(i + initialKey, payload, &man);
 	}
 
 	// check the whole damn thing. We should have 
 	// MAX_DELTA_CHAIN insert deltas.
 	//
 	// get the first in chain as indicated by findNode.
-	Node* currNode = t->findNode(MAX_DELTA_CHAIN - 1,
+	Node* currNode = t.findNode(MAX_DELTA_CHAIN - 1,
 		&man).node;
 
 	// check that it is the correct node.
-	Node* root = t->map_->get(t->rootPid_);
-	Node* firstInChain = ((IndexNode*) root)->getIndexPid(0);
+	Node* root = t.map_->get(t.rootPid_);
+	Node* firstInChain = t.map_->get(((IndexNode*) root)->getIndexPID(0));
 	EXPECT_EQ(firstInChain, currNode);
 
 	byte* foundPayload;
@@ -120,5 +120,6 @@ TEST(findNodeTest) {
 }
 
 int main(int argc, char** argv) {
-  findNodeTest();
+	dataNodeInsertConsolidateTest();
+ 	findNodeTest();
 }
